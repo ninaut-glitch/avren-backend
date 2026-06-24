4d373d76fefb56b19eabaa2cd3eb94572674263f
ninaut-glitch <ninaut@me.com>
2026-06-24 10:20:12 -0300

    Update interactions.module.ts

--- avren_backend/src/modules/interactions/interactions.module.ts
+++ avren_backend/src/modules/interactions/interactions.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { InteractionsController } from './interactions.controller';
+import { LeadInteractionsController } from './lead-interactions.controller';
 import { InteractionsService } from './interactions.service';
 import { InteractionsRepository } from './interactions.repository';
 
 @Module({
-  controllers: [InteractionsController],
+  controllers: [InteractionsController, LeadInteractionsController],
   providers: [InteractionsService, InteractionsRepository],
   exports: [InteractionsService],
 })
