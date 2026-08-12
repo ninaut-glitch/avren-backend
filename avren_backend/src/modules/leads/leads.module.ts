8a42960f34047e63e6cb57c57b6b048fa8bfb81e
ninaut-glitch <ninaut@me.com>
2026-08-12 11:17:31 -0300

    feat(leads): registra WebsiteLeadsController no modulo

--- avren_backend/src/modules/leads/leads.module.ts
+++ avren_backend/src/modules/leads/leads.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LeadsController } from './leads.controller';
+import { WebsiteLeadsController } from './website-leads.controller';
 import { LeadsService } from './leads.service';
 import { LeadsRepository } from './leads.repository';
 
 @Module({
-  controllers: [LeadsController],
+  controllers: [LeadsController, WebsiteLeadsController],
   providers: [LeadsService, LeadsRepository],
   exports: [LeadsService],
 })
